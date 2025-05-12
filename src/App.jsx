b94749aac1a4793ef7f5acffda1b55f3d2c2644c
elixir-dhruvil <dhruvil.cloud12@gmail.com>
2025-05-12 18:08:30 +0530

    Project Setup using react, tailwind css and other dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center bg-gray-100 p-4 rounded-md shadow-md">
+      <h1 className="text-2xl font-bold text-gray-800">pagar_book</h1>
+    </div>
+  )
+}
+
+export default App
