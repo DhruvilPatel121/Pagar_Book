dd2ddce95806778889c14959e7d2099fd23dddbe
elixir-dhruvil <dhruvil.cloud12@gmail.com>
2025-05-13 15:55:20 +0530

    Update layout and pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import './App.css'
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
 function App() {
-
-  return (
-    <div className="flex flex-col items-center bg-gray-100 p-4 rounded-md shadow-md">
-      <h1 className="text-2xl font-bold text-gray-800">pagar_book</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
